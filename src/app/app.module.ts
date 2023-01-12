c33051a10491bc52a216e09ae5b76263de463767
eliassommer <60005105+eliassommer@users.noreply.github.com>
2023-01-12 10:17:12 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { GaugeModule} from 'angular-gauge';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
